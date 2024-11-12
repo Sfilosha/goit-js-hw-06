efb39eb4f7c978f377a3b4d143fbb36877ccf620
Sfilosha <sfilosha@gmail.com>
2024-11-12 13:54:50 +0200

    complete tasks

--- js/task-2.js
+++ js/task-2.js
@@ -1,5 +1,35 @@
 // Створи клас Storage, який створюватиме об'єкти для управління складом товарів. Клас очікує лише один аргумент — початковий масив товарів, який записується до створеного об'єкта в приватну властивість items.
 
+class Storage {
+    #items = [];
+
+    constructor(params) {
+        this.#items = params;
+    }
+
+    getItems() {
+        return this.#items;
+    }
+
+    addItem(newItem) {
+        this.#items.push(newItem);
+    }
+
+    removeItem(itemToRemove) {
+        let newArray = [];
+
+        for (const item of this.#items) {
+            // console.log(`Checking if ${itemToRemove} is equal to ${item}`)
+            if (item !== itemToRemove) {
+                // console.log(`${itemToRemove} is not queal. Added ${item} to array`)
+                newArray.push(item);
+            }
+        }
+
+        this.#items = newArray;
+    }
+}
+
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 storage.addItem("Droid");
--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,26 @@
 // Напиши клас StringBuilder, який приймає один параметр initialValue — довільний рядок, який записується у приватну властивість value об'єкта, що створюється.
 
+class StringBuilder {
+    #value;
+
+    constructor(initialValue) {
+        this.#value = initialValue;
+    }
+
+    getValue() {
+        return this.#value;
+    }
+    padEnd(str) {
+        this.#value = this.#value + str;
+    }
+    padStart(str) {
+        this.#value =  str + this.#value;
+    }
+    padBoth(str) {
+        this.#value = str + this.#value + str;
+    }
+};
+
 const builder = new StringBuilder(".");
 console.log(builder.getValue()); // "."
 builder.padStart("^");
